c0d15d917726e2bcf015dcff36a4786a4cbeef04
esickert <esickert@gmail.com>
2016-07-17 13:29:43 -0700

    more

--- src/CirclePackage/TestCircle.java
+++ src/CirclePackage/TestCircle.java
@@ -4,7 +4,7 @@ public class TestCircle {
 	/** main method */
 	public static void main(String[] args) {
 	//Create  a circle with radius 5.0
-	Circle myCircle = new Circle(6.0);
+	Circle myCircle = new Circle(5.0);
 	System.out.println("The area of the circle of radius "
 		+ myCircle.getRadius() + " is " + myCircle.findArea());
 	}
