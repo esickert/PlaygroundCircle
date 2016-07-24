7769024a27a2c58177126d3b7a9fa225e83f2ab2
Erich Sickert <esickert@gmail.com>
2016-07-24 13:25:06 -0700

    test eclipse

--- src/CirclePackage/TestCircle.java
+++ src/CirclePackage/TestCircle.java
@@ -3,8 +3,8 @@ package CirclePackage;
 public class TestCircle {
 	/** main method */
 	public static void main(String[] args) {
-	//Create  a circle with radius 5.0
-	Circle myCircle = new Circle(17.0);  //instance of circle
+	//Create  a circle with radius x.x
+	Circle myCircle = new Circle(5.0);  //instance of circle
 	System.out.println("The area of the circle of radius "
 		+ myCircle.getRadius() + " is " + myCircle.findArea());
 //*************************************************************		
